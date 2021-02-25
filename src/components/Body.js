a8d52f8f9f31122a89cd1ffe6a67bd1c0ad6e192
Juan Pablo Chica <juanpchica@pdc.magnetron.com>
2021-02-25 09:57:34 -0500

    Se agrega componentes iniciales para body y header y moldes

---
+++ src/components/Body.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export const Body = () => {
+    return (
+        <section>
+            <Moldes/>
+        </section>
+    )
+}
+
+const Moldes = () => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>
+                        Dimensiones
+                    </th>
+                </tr>
+
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Ok</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
