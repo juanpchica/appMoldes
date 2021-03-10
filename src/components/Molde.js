d43fa1249deb5a527c775dceea4bd5a031e9e288
Juan Pablo Chica <juanpchica@hotmail.com>
2021-03-10 10:16:27 -0500

    Se agrega onchange en inputs

--- src/components/Molde.js
+++ src/components/Molde.js
@@ -1,14 +1,14 @@
 import React,{ useState,useEffect } from 'react'
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
-import { Button,Col,Form } from 'react-bootstrap';
+import { Button,Col,Form,Row } from 'react-bootstrap';
 
 export const Molde = () => {
 const { id } = useParams();
 
 const apiURL = 'http://localhost:8084/apiMoldes/api/moldes?id='+id;
     
-    const [molde,setMolde] = useState({});
+    const [molde,setMolde] = useState({dimensiones:"",columna:"",lado:"",tipo:"",ubicacion:"",cantidad:"",id:id});
     const [isLoading,setIsLoading] = useState(true)
     
     //Fetch data from server
@@ -16,7 +16,6 @@ const apiURL = 'http://localhost:8084/apiMoldes/api/moldes?id='+id;
         const response = await fetch(apiURL);
         const molde = await response.json();
          
-        console.log(molde);
         setIsLoading(false);
         setMolde(molde[0]); 
     }
@@ -29,42 +28,58 @@ const apiURL = 'http://localhost:8084/apiMoldes/api/moldes?id='+id;
 
     return (
         <section className="content-form-molde">
-            <h2>Molde ID: {molde.id}</h2>
+            
+            <Row>
+                <Col><h2>Molde ID: {molde.id}</h2></Col>
+                <Col className="text-right">
+                    <Link to="/" className="btn btn-danger">
+                        Regresar
+                    </Link>
+                </Col>
+            </Row>
             <Form>
                 <Form.Row>
                     <Form.Group as={Col}>
                         <Form.Label>Dimensiones</Form.Label>
-                        <Form.Control type="text" value={molde.dimensiones} />
+                        <Form.Control type="text" value={molde.dimensiones} onChange={(e)=>{setMolde({...molde,dimensiones:e.target.value})}} />
                     </Form.Group>
 
                     <Form.Group as={Col}>
                         <Form.Label>Tipo</Form.Label>
-                        <Form.Control type="text" value={molde.tipo}/>
+                        <Form.Control type="text" value={molde.tipo} onChange={(e)=>{setMolde({...molde,tipo:e.target.value})}} />
                     </Form.Group>
                 </Form.Row>
 
                 <Form.Row>
                     <Form.Group as={Col}>
                         <Form.Label>Cantidad</Form.Label>
-                        <Form.Control type="text" placeholder="Cantidad de Moldes: "/>
+                        <Form.Control type="text" placeholder="Cantidad de Moldes:" value={molde.cantidad} onChange={(e)=>{setMolde({...molde,cantidad:e.target.value})}}/>
                     </Form.Group>
 
                     <Form.Group as={Col}>
                         <Form.Label>Lado</Form.Label>
-                        <Form.Control type="text" placeholder="Ej: A,B,C" />
+                        <Form.Control type="text" placeholder="Ej: A,B,C" value={molde.lado} onChange={(e)=>{setMolde({...molde,lado:e.target.value})}}/>
                     </Form.Group>
 
                     <Form.Group as={Col}>
                         <Form.Label>Columna</Form.Label>
-                        <Form.Control type="text" placeholder="Ej: 1,2,3" />
+                        <Form.Control type="text" placeholder="Ej: 1,2,3" value={molde.columna} onChange={(e)=>{setMolde({...molde,columna:e.target.value})}}/>
                     </Form.Group>
                 </Form.Row>
 
                 <Form.Row>
-                    <Form.Group>
+                    <Form.Group as={Col}>
+                        <Form.Check type="checkbox" label="Boquete" />
+                    </Form.Group>
+
+                    <Form.Group as={Col}>
+                        <Form.Check type="checkbox" label="Soporte" />
+                    </Form.Group>
+
+                    <Form.Group as={Col}>
                         <Form.Check type="checkbox" label="Activo" />
                     </Form.Group>
-                </Form.Row>
+                </Form.Row> 
                 
                 <Button variant="primary" type="submit">
                     Guardar
