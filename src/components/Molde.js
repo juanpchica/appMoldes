c3b6dd81afd09b62b10d0d36fa8d56a169a15834
Juan Pablo Chica <juanpchica@hotmail.com>
2021-03-10 15:40:35 -0500

    Update with form method added

--- src/components/Molde.js
+++ src/components/Molde.js
@@ -4,16 +4,16 @@ import { useParams, Link } from "react-router-dom";
 import { Button,Col,Form,Row } from 'react-bootstrap';
 
 export const Molde = () => {
-const { id } = useParams();
+    const { id } = useParams();
 
-const apiURL = 'http://localhost:8084/apiMoldes/api/moldes?id='+id;
+    const apiURL = 'http://localhost:8084/apiMoldes/api/moldes';
     
     const [molde,setMolde] = useState({dimensiones:"",columna:"",lado:"",tipo:"",ubicacion:"",cantidad:"",id:id});
     const [isLoading,setIsLoading] = useState(true)
     
     //Fetch data from server
     const fetchMolde = async() => {
-        const response = await fetch(apiURL);
+        const response = await fetch(apiURL+"?id="+id);
         const molde = await response.json();
          
         setIsLoading(false);
@@ -23,6 +23,22 @@ const apiURL = 'http://localhost:8084/apiMoldes/api/moldes?id='+id;
     useEffect(()=>{
         fetchMolde();
     },[])
+
+    const updateMolde = (e) => {
+        e.preventDefault();
+
+        const requestOptions = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify(molde)
+        };
+        fetch(apiURL+'/actualizar', requestOptions)
+            .then(response =>{
+                console.log(response);
+                response.json()
+            })
+            .then(data => console.log("data"+ data));
+    }
 
     if(isLoading) return <div>Cargando datos...</div>
 
@@ -37,23 +53,23 @@ const apiURL = 'http://localhost:8084/apiMoldes/api/moldes?id='+id;
                     </Link>
                 </Col>
             </Row>
-            <Form>
+            <Form onSubmit={updateMolde}>
                 <Form.Row>
                     <Form.Group as={Col}>
                         <Form.Label>Dimensiones</Form.Label>
-                        <Form.Control type="text" value={molde.dimensiones} onChange={(e)=>{setMolde({...molde,dimensiones:e.target.value})}} />
+                        <Form.Control type="text" value={molde.dimensiones} onChange={(e)=>{setMolde({...molde,dimensiones:e.target.value})}} disabled/>
                     </Form.Group>
 
                     <Form.Group as={Col}>
                         <Form.Label>Tipo</Form.Label>
-                        <Form.Control type="text" value={molde.tipo} onChange={(e)=>{setMolde({...molde,tipo:e.target.value})}} />
+                        <Form.Control type="text" value={molde.tipo} onChange={(e)=>{setMolde({...molde,tipo:e.target.value})}} disabled />
                     </Form.Group>
                 </Form.Row>
 
                 <Form.Row>
                     <Form.Group as={Col}>
                         <Form.Label>Cantidad</Form.Label>
-                        <Form.Control type="text" placeholder="Cantidad de Moldes:" value={molde.cantidad} onChange={(e)=>{setMolde({...molde,cantidad:e.target.value})}}/>
+                        <Form.Control type="number" placeholder="Cantidad de Moldes:" value={molde.cantidad} onChange={(e)=>{setMolde({...molde,cantidad:e.target.value})}}/>
                     </Form.Group>
 
                     <Form.Group as={Col}>
@@ -69,21 +85,24 @@ const apiURL = 'http://localhost:8084/apiMoldes/api/moldes?id='+id;
 
                 <Form.Row>
                     <Form.Group as={Col}>
-                        <Form.Check type="checkbox" label="Boquete" />
+                        <Form.Check type="checkbox" label="Boquete" defaultChecked={molde.boquete} onChange={()=>{setMolde({...molde,boquete:!molde.boquete})}}/>
                     </Form.Group>
 
                     <Form.Group as={Col}>
-                        <Form.Check type="checkbox" label="Soporte" />
+                        <Form.Check type="checkbox" label="Soporte" defaultChecked={molde.soporte} onChange={()=>{setMolde({...molde,soporte:!molde.soporte})}}/>
                     </Form.Group>
 
                     <Form.Group as={Col}>
-                        <Form.Check type="checkbox" label="Activo" />
+                        <Form.Check type="checkbox" label="Activo" defaultChecked={molde.estado} onChange={()=>{setMolde({...molde,estado:!molde.estado})}}/>
                     </Form.Group>
                 </Form.Row> 
-                
-                <Button variant="primary" type="submit">
-                    Guardar
-                </Button>
+                <Row>
+                    <Col className="text-center">
+                        <Button variant="primary" type="submit">
+                            Guardar
+                        </Button>
+                    </Col>
+                </Row>
             </Form>
         </section>
     )
