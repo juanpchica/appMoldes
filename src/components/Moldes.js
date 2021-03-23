a241b36693c04c0cc45e8f3e5c496721de158294
Juan Pablo Chica <juanpchica@hotmail.com>
2021-03-23 11:46:27 -0500

    New package icon added, link added to edit and table organziced

--- src/components/Moldes.js
+++ src/components/Moldes.js
@@ -1,9 +1,12 @@
 import React, { useState,useEffect } from 'react'
 import { AgGridColumn, AgGridReact } from 'ag-grid-react';
+import { ArrowRight, PencilSquare } from 'react-bootstrap-icons';
+
 
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 export const Moldes = () => {
     const apiURL = 'http://localhost:8084/apiMoldes/api/moldes'
@@ -26,7 +29,7 @@ export const Moldes = () => {
     }
 
     const cellRenderButton = (props)=>{
-        return <Button onClick={()=>{ver(props.value)}}>Click</Button>
+        return <Link to={"/molde/"+props.value}><PencilSquare color="gray" size={25} /></Link>
     }
 
     const ver = (p)=>{
@@ -46,11 +49,11 @@ export const Moldes = () => {
                 paginationAutoPageSize={true}
                 frameworkComponents={frameworkComponents}
                 >
-                <AgGridColumn field="ID" sortable={ true } filter={false } resizable={true} flex={1} cellRenderer="cellRenderButton" ></AgGridColumn>
-                <AgGridColumn field="dimensiones" sortable={ true } filter={true } resizable={true} flex={1} ></AgGridColumn>
-                <AgGridColumn field="cantidad" sortable={ true } filter={true }  resizable={true} flex={1}></AgGridColumn>
-                <AgGridColumn field="ubicacion" sortable={ true } filter={true }  resizable={true} flex={1}></AgGridColumn>
-                <AgGridColumn field="tipo" sortable={ true } filter={true }  resizable={true} flex={1}></AgGridColumn>
+                <AgGridColumn field="dimensiones" headerName="DIMENSIONES" sortable={ true } filter={true } resizable={true} flex={1} ></AgGridColumn>
+                <AgGridColumn field="cantidad" headerName="CANTIDAD" sortable={ true } filter={true }  resizable={true} flex={1}></AgGridColumn>
+                <AgGridColumn field="ubicacion" headerName="UBICACIÓN" sortable={ true } filter={true }  resizable={true} flex={1}></AgGridColumn>
+                <AgGridColumn field="tipo" headerName="TIPO" sortable={ true } filter={true }  resizable={true} flex={1}></AgGridColumn>
+                <AgGridColumn field="ID" headerName="EDITAR"sortable={ true } width="100" filter={false } resizable={true} cellRenderer="cellRenderButton" ></AgGridColumn>
             </AgGridReact>
         </div>
     )
